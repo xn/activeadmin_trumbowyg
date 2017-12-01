49542175739c97c2c9b214b4f9c73c981f01de23
xn <christian.trosclair@gmail.com>
2017-12-01 09:11:08 -0600

    make usable with polymorphic and edge

--- app/assets/javascripts/activeadmin/trumbowyg_input.js
+++ app/assets/javascripts/activeadmin/trumbowyg_input.js
@@ -5,3 +5,19 @@ $(document).ready( function() {
     $(this).trumbowyg( options );
   });
 });
+
+  $(document).on('polymorphic_has_many_form:inserted', '.polymorphic_has_many_container', function(event, fieldset) {
+    return $('.trumbowyg-textarea').each(function () {
+    var options = {};
+    options = $.extend({}, options, $(this).data( 'options' ));
+    $(this).trumbowyg( options );
+  });
+  });
+
+  $(document).on('has_many_form:inserted', '.has_many_container', function(event, fieldset) {
+    return $('.trumbowyg-textarea').each(function () {
+    var options = {};
+    options = $.extend({}, options, $(this).data( 'options' ));
+    $(this).trumbowyg( options );
+  });
+  });
